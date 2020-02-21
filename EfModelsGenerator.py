8ab62ebf4c0913c73df7acfd18ca32b5e40aea4c
ZhekaCher <zhekacher20.05@gmail.com>
2020-02-22 01:38:39 +0600

    Done v1.0 release

--- EfModelsGenerator.py
+++ EfModelsGenerator.py
@@ -1,4 +1,5 @@
 types = {'integer': 'int?',
+         'bigint': 'long?',
          'text': 'string',
          'timestamp': 'DateTime?',
          'boolean': 'bool?',
@@ -7,19 +8,23 @@ types = {'integer': 'int?',
 
 
 class ClassElement:
+    key = False
     name = ''
     type = 'unknown'
 
 
 def main():
-    template = 'namespace DataMigrationSystem.Models.Web.TradingFloor' \
-               '{' \
-               '[Table("$TABLE_NAME")]' \
-               'public class $CLASS_NAME' \
-               '{' \
-               '$CONTAINER' \
-               '}' \
-               '}'
+    template = 'using System;\n' \
+               'using System.ComponentModel.DataAnnotations;\n' \
+               'using System.ComponentModel.DataAnnotations.Schema;\n\n' \
+               'namespace SomeNamespace\n' \
+               '{\n' \
+               '\t[Table("$TABLE_NAME")]\n' \
+               '\tpublic class $CLASS_NAME\n' \
+               '\t{\n' \
+               '$CONTAINER\n' \
+               '\t}\n' \
+               '}\n'
 
     import sys
     file = open(sys.argv[1], 'r', encoding='utf-8')
@@ -33,26 +38,38 @@ def main():
         .replace(' ', '') \
         .split('.')
     table_name = pre_table_name[len(pre_table_name) - 1]  # Ready
-    template = template.replace('$TABLE_NAME', table_name)
-    template = template.replace('$CLASS_NAME', modify_to_camel(table_name))
 
-    # fields_creating
+    # fields_parsing
     inner_fields = text[text.index('(') + 1:text.index(')')].replace('\n', '').split(',')
     class_fields = []
     for inner_field in inner_fields:
         inner_field = inner_field.strip()
         class_field = ClassElement()
         class_field.name = inner_field.split(' ')[0]
+        if inner_field.__contains__('primary key'):
+            class_field.key = True
         for t in types:
             if inner_field.__contains__(t):
                 class_field.type = t
                 break
         class_fields.append(class_field)
-    return template
 
+    # generating result
+    template = template.replace('$TABLE_NAME', table_name)
+    template = template.replace('$CLASS_NAME', modify_to_camel(table_name))
+    container = '\t\t'
+    for class_field in class_fields:
+        if class_field.key:
+            container += '[Key] '
+        container += '[Column(\"' + class_field.name + '\")] public ' \
+                     + types[class_field.type] \
+                     + ' ' \
+                     + modify_to_camel(class_field.name) \
+                     + '{get; set;}\n\t\t'
+    template = template.replace('$CONTAINER', container)
+    file = open(modify_to_camel(table_name) + ".cs", 'w', encoding='utf-8')
+    file.write(template)
 
-# for field in inner_fields:
-#     print(field+"\n")
 
 def modify_to_camel(text):
     text = text[0].upper() + text[1:]
@@ -62,4 +79,4 @@ def modify_to_camel(text):
     return text
 
 
-print(main())
+main()
