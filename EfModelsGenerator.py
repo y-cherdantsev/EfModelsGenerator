97fa05270ef0ff8802829db5ed1af4dff30802d1
ZhekaCher <zhekacher20.05@gmail.com>
2020-02-22 01:14:40 +0600

    class fields parsed

--- EfModelsGenerator.py
+++ EfModelsGenerator.py
@@ -1,3 +1,16 @@
+types = {'integer': 'int?',
+         'text': 'string',
+         'timestamp': 'DateTime?',
+         'boolean': 'bool?',
+         'double precision': 'double?',
+         'unknown': 'UnknownType'}
+
+
+class ClassElement:
+    name = ''
+    type = 'unknown'
+
+
 def main():
     template = 'namespace DataMigrationSystem.Models.Web.TradingFloor' \
                '{' \
@@ -12,7 +25,7 @@ def main():
     file = open(sys.argv[1], 'r', encoding='utf-8')
     text = file.read()
 
-    #table_naming
+    # table_naming
     pre_table_name = text[0:text.index('(')] \
         .replace('\n', '') \
         .replace('create', '') \
@@ -23,8 +36,18 @@ def main():
     template = template.replace('$TABLE_NAME', table_name)
     template = template.replace('$CLASS_NAME', modify_to_camel(table_name))
 
-    #fields_creating
+    # fields_creating
     inner_fields = text[text.index('(') + 1:text.index(')')].replace('\n', '').split(',')
+    class_fields = []
+    for inner_field in inner_fields:
+        inner_field = inner_field.strip()
+        class_field = ClassElement()
+        class_field.name = inner_field.split(' ')[0]
+        for t in types:
+            if inner_field.__contains__(t):
+                class_field.type = t
+                break
+        class_fields.append(class_field)
     return template
 
 
@@ -34,7 +57,7 @@ def main():
 def modify_to_camel(text):
     text = text[0].upper() + text[1:]
     while text.__contains__('_'):
-        text = text[:text.index('_')] + text[text.index('_')+1:][0].upper() + text[text.index('_')+2:]
+        text = text[:text.index('_')] + text[text.index('_') + 1:][0].upper() + text[text.index('_') + 2:]
         continue
     return text
 
